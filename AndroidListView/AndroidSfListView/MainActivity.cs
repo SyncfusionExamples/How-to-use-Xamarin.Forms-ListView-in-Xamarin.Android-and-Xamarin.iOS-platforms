ec59c7431bfc195a99c631893f37b47cb031ffa2
Rawoof <rawoofsharief.muthujasherif@syncfusion.com>
2018-11-08 16:12:59 +0530

    Sample commit

---
+++ AndroidListView/AndroidSfListView/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using Android.Support.V7.App;
+using Fragment = Android.App.Fragment;
+using Xamarin.Forms.Platform.Android;
+using Android.Support.V4.App;
+
+namespace AndroidSfListView
+{
+    [Activity(Label = "AndroidSfListView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public static MainActivity Instance;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Forms.Init(this, bundle);
+            Instance = this;
+
+            SetContentView(Resource.Layout.Main);
+
+            SetContentView(Resource.Layout.Main);
+           
+            var mainPage = new SfListViewPage().CreateFragment(this);
+            FragmentManager
+           .BeginTransaction()
+           .Replace(Resource.Id.fragment_frame_layout, mainPage)
+           .Commit();
+        }
+    }
+}
+
---
+++ AndroidListView/AndroidSfListView/ViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace AndroidSfListView
+{
+    class ViewModel
+    {
+
+        #region Properties
+
+        public ObservableCollection<Contacts> contactsinfo { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public ViewModel()
+        {
+            contactsinfo = new ObservableCollection<Contacts>();
+            Random r = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                var contact = new Contacts();
+                contact.ContactName = CustomerNames[r.Next(0, 4)];
+                contact.ContactNumber = r.Next(720, 799).ToString() + " - " + r.Next(3010, 3999).ToString();
+                contact.ContactColor = Color.FromRgb(r.Next(40, 255), r.Next(40, 255), r.Next(40, 255));
+                contactsinfo.Add(contact);
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        public string[] CustomerNames = new string[] {
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+    };
+        #endregion
+    }
+
+}
